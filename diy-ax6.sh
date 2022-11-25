8a8d5627b74eb6fcff25969814f9a55ec6f7f677
aoxueos <78366100+aoxueos@users.noreply.github.com>
2022-11-25 15:34:32 +0800

    Update diy-ax6.sh

--- diy-ax6.sh
+++ diy-ax6.sh
@@ -42,7 +42,8 @@ git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-git clone https://github.com/sirpdboy/luci-app-advanced
+git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
+
 
 #添加smartdns
 git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter

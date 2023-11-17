a9f827d07395ee76ad00d4fa79bfbf71e7c81c50
梦海Yumekai <78366100+YumesomeZakura@users.noreply.github.com>
2023-11-18 00:57:44 +0800

    Add files via upload

---
+++ diy/lean2.sh
@@ -0,0 +1,18 @@
+#补充汉化
+echo -e "\nmsgid \"Control\"" >> feeds/luci/modules/luci-base/po/zh-cn/base.po
+echo -e "msgstr \"控制\"" >> feeds/luci/modules/luci-base/po/zh-cn/base.po
+
+echo -e "\nmsgid \"NAS\"" >> feeds/luci/modules/luci-base/po/zh-cn/base.po
+echo -e "msgstr \"网络存储\"" >> feeds/luci/modules/luci-base/po/zh-cn/base.po
+
+
+##补充汉化
+echo -e "\nmsgid \"General\"" >> package/feeds/luci/luci-app-dnsforwarder/po/zh-cn/dnsforwarder.po
+echo -e "msgstr \"常规\"" >> package/feeds/luci/luci-app-dnsforwarder/po/zh-cn/dnsforwarder.po
+
+echo -e "\nmsgid \"LOG\"" >> package/feeds/luci/luci-app-dnsforwarder/po/zh-cn/dnsforwarder.po
+echo -e "msgstr \"日志\"" >> package/feeds/luci/luci-app-dnsforwarder/po/zh-cn/dnsforwarder.po
+
+              
+##配置ip等
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
